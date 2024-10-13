36ef894332ae94fe5dea48e9b618fdef8c43745a
ahmad123jamal <you@example.com>
2024-10-13 12:35:23 +0500

    more js code are added

--- script.js
+++ script.js
@@ -6,18 +6,76 @@ let myPet = {
     hunger : 60,
 
     feed : function () {
-        console.log("your happiness is ${this.hunger}")
-        this.hunger = 0;
+        if (this.hunger > 0) {
+        this.hunger -= 20;
+        if (this.hunger < 0) {
+            this.hunger = 0;
+        }
+        alert(`${this.name} has been fed! Hunger is now ${this.hunger}.`);
+    } else {
+        alert(`${this.name} is not hungry!`);
+    }
+    
     },
 
-    play : function() {
-        this.happiness = this.happiness +1;
-        if (this.happiness > 100) {
-            console.log("your are too much happy")
-        } else {
-            console.log("you are happy")
-        }
+     play: function () {
+    this.happiness += 20;
+    if (this.happiness > 100) {
+        this.happiness = 100;
+        alert(`${this.name} is too happy!`);
+    } else {
+        alert(`${this.name} is happy! Happiness is now ${this.happiness}.`);
     }
+},
 
+    agePet: function () {
+        this.age += 1;
+        this.happiness -= 5;
+        this.hunger += 10;
+        
+        // Ensure happiness doesn't go below 0
+        if (this.happiness < 0) {
+            this.happiness = 0;
+        }
 
+        alert(`${this.name} is now ${this.age} years old. Happiness is now ${this.happiness}, and hunger is now ${this.hunger}.`);
+    },
+
+    info: function () {
+        alert(`Pet Name: ${this.name}\nType: ${this.type}\nAge: ${this.age}\nHappiness: ${this.happiness}\nHunger: ${this.hunger}`);
+    }
 }
+
+// Function to interact with the pet
+
+function interactWithPet() {
+    let action;
+    do {
+        action = prompt("What would you like to do? (feed, play, age, info, exit)");
+
+        switch (action) {
+            case "feed":
+                myPet.feed();
+                break;
+            case "play":
+                myPet.play();
+                break;
+            case "age":
+                myPet.agePet();
+                break;
+            case "info":
+                myPet.info();
+                break;
+            case "exit":
+                alert("Goodbye!");
+                break;
+            default:
+                alert("Invalid action! Please choose 'feed', 'play', 'age', 'info', or 'exit'.");
+        }
+    } while (action !== "exit");
+}
+
+
+
+// Start the interaction
+interactWithPet();
